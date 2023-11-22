9edad9de88277716a6db8c3608dab5d0af46064f
elenapltnkv <elenapltnkv@gmail.com>
2023-11-22 17:38:13 +0300

    stage2 commit

---
+++ src/main/java/SecondTestClass.java
@@ -0,0 +1,18 @@
+public class SecondTestClass {
+    public static void main(String[] args) {
+        int a=3;
+        int b = 3;
+        System.out.println(a+b);
+
+        numNumber(5, 8);
+        System.out.println("lalalalalala");
+
+    }
+    public static void numNumber(int b, int c){
+        if(b>c){
+            System.out.println("AAAAAAAAAAAAAAAA");
+        }else {
+            System.out.println("BBBBBBBBBBBBBBB");
+        }
+    }
+}
